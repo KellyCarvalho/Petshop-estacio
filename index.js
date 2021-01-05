b8349402af3c3a3676c3c5ab3d626e7ecd307f2b
KellyCarvalho <18kellycarvalho@gmail.com>
2021-01-04 21:55:47 -0300

    Adicionando delete e update

--- index.js
+++ index.js
@@ -1,101 +1,163 @@
-    const express = require("express");
-    const app = express();
-    const bodyParser = require("body-parser");
-    const connection =require("./database/database"); 
-    const Cliente = require("./database/cliente");
-    const Sequelize = require("sequelize");
-    const operadoresSql = Sequelize.Op;
+            const express = require("express");
+            const app = express();
+            const bodyParser = require("body-parser");
+            const connection =require("./database/database"); 
+            const Cliente = require("./database/cliente");
+            const Sequelize = require("sequelize");
+            const operadoresSql = Sequelize.Op;
+            const jsdom = require("jsdom");
+            const { JSDOM } = jsdom;
 
 
-
-
-
-
-    //database
-    connection
-            .authenticate()
-            .then(()=>{
-    console.log("conexão feita com sucesso");
-            })
-            .catch((msgerro)=>{
-    console.log(msgerro);
-            });
-
-    app.use(bodyParser.urlencoded({extended:false}));
-    app.use(bodyParser.json());
-    app.set('view engine','ejs');
-
-
-
-    app.get('/',(req,res)=>{
-
-
-    Cliente.findAll({
-        raw:true,
-        order:[
-            ['id','DESC']
-        ]
-    }).then((clientes=>{
-        res.render("index",{
-            clientes:clientes
-
-        });
         
+
+
+
+
+
+
         
-    }));
 
 
 
-    });
 
 
-    app.post('/pesquisar',(req,res)=>{
-            
-            var nome = req.body.nome;
-        
-            
+            //database
+            connection
+                    .authenticate()
+                    .then(()=>{
+            console.log("conexão feita com sucesso");
+                    })
+                    .catch((msgerro)=>{
+            console.log(msgerro);
+                    });
+
+            app.use(bodyParser.urlencoded({extended:false}));
+            app.use(bodyParser.json());
+            app.set('view engine','ejs');
+
+
+
+            app.get('/',(req,res)=>{
+
+
             Cliente.findAll({
-                where:{
-                    nome:{
-                        [operadoresSql.like]:nome
-                    }
-                }
-                    
-            
-            
-            }).then((clientes)=>{
-
-            
+                raw:true,
+                order:[
+                    ['id','DESC']
+                ]
+            }).then((clientes=>{
                 res.render("index",{
                     clientes:clientes
-                })
+
+                });
+                
+                
+            }));
+
+
+
+            });
+
+
+            app.post('/pesquisar',(req,res)=>{
+                    
+                    var nome = req.body.nome;
+                
+                    
+                    Cliente.findAll({
+                        where:{
+                            nome:{
+                                [operadoresSql.like]:nome
+                            }
+                        }
+                            
+                    
+                    
+                    }).then((clientes)=>{
+
+                    
+                        res.render("index",{
+                            clientes:clientes
+                        })
+                    })
+                
+                    });
+
+
+
+            
+
+
+
+            app.post('/salvarCliente',(req,res)=>{
+                var nome = req.body.nome;
+                var telefone=req.body.telefone;
+            
+
+                Cliente.create({
+            
+                    nome:nome,
+                    telefone:telefone
+                }).then(()=>{
+                    res.redirect("./");
+                }).catch({
+                
+                }).then((clientes)=>{
+                    res.render("index",{
+                        clientes:clientes
+                    });
+                });
+
+
+            });
+
+
+            app.get('/alterar/:id',(req,res)=>{
+
+              
+                var id = req.params.id;
+        Cliente.findByPk(id).then((cliente)=>{
+            res.render("alterar", {
+                cliente:cliente
             })
+
+
+    
+        });
+         
+        var nome = req.body.nome;
+        var telefone = req.body.telefone;
+        console.log(nome, telefone);
         
+
+        Cliente.update({nome:nome,telefone:telefone},{
+            where:{id:id}
+            }).then(()=>{
+                console.log("Alteração feita com sucesso");
+
+            });
+
+    
+
+   
+        });
+
+        
+
+            app.get('/deletarCliente/:id',(req,res)=>{
+                var id = req.params.id;
+                Cliente.destroy({
+                    where:{
+                    id:id
+                    }
+                }).then((cliente)=>{
+                  console.log("deletado");
+                  res.redirect("./");
+                })
+
             });
 
 
 
-    
-
-
-
-    app.post('/salvarCliente',(req,res)=>{
-    var nome = req.body.nome;
-    var tel = req.body.telefone;
-
-        Cliente.create({
-    
-            nome:nome,
-            telefone:tel
-        }).then(()=>{
-            res.redirect("./");
-        }).catch({
-        
-        });
-
-
-    });
-
-
-
-    app.listen(8080,()=>{console.log("...");});
+            app.listen(8080,()=>{console.log("...");});
